ad04c6ed351493ba84d42753859d747f53d3fb5e
Joseph Gyegyiri <61208810+JosephGyegyiri@users.noreply.github.com>
2023-10-16 17:23:05 -0400

    Update Webapp.py

--- Webapp.py
+++ Webapp.py
@@ -14,12 +14,30 @@ markdown = """
 Welcome to the Atlanta Crime Cluster Dashboard! 🚓🔍
 
 This interactive dashboard provides insights into crime data in Atlanta, Georgia. The data is sourced from the Atlanta Police Department (2020 data) and is presented in a visually appealing and informative way.
-"""
 
-#st.sidebar.title("About")
+## About This App
+
+This app is designed to help you better understand crime patterns in Atlanta and make informed decisions. It's a valuable tool for law enforcement, city planners, and anyone interested in the safety of Atlanta.
+
+Feel free to explore the various features and analyses provided in this dashboard. If you have any questions or feedback, please don't hesitate to reach out.
+
+Enjoy your exploration! (Joseph Gyegyiri, 2022)
+"""
+markdown2 = """
+         Joseph Gyegyiri is a graduate student of Florida Atlantic University, pursuing a masters in civil engineering with a concentartion in Geomatics and Transportation Engineering.
+         He aspire to be a geospatial analyst/developer where he would utilize geospatial science and engineering for a better world
+         """
+
+
+st.sidebar.title("About")
 st.sidebar.markdown(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
+
+st.sidebar.write("About Author")
+logo2 = 'https://media.licdn.com/dms/image/D4E03AQHUL3mFEUHptg/profile-displayphoto-shrink_800_800/0/1668234298036?e=1697673600&v=beta&t=ZTWrHw786rKyoAWueLxq2BBg3MjHTEBZqAj5ahRz7uE'
+st.sidebar.image(logo2)
+st.sidebar.markdown(markdown2)
 
 st.title("Atlanta Crime Cluster")
 
